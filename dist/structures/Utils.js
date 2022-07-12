d6ec87e3b0a058bdae005e8715b3f7e6741da2f4
Tomato6966 <chris.pre03@gmail.com>
2022-07-12 21:52:56 +0200

    little fix for unresolved tracks title for descr

--- dist/structures/Utils.js
+++ dist/structures/Utils.js
@@ -170,7 +170,7 @@ class TrackUtils {
                 });
                 if (originalAudio) {
                     originalAudio.uri = unresolvedTrack.uri;
-                    if(originalAudio.title == 'Unknown title' && originalAudio.title != unresolvedTrack.title) originalAudio.title = unresolvedTrack.title;
+                    if((originalAudio.title == 'Unknown title' || originalAudio.title == "Unspecified description") && originalAudio.title != unresolvedTrack.title) originalAudio.title = unresolvedTrack.title;
                     if(originalAudio.author != unresolvedTrack.author) originalAudio.author = unresolvedTrack.author;
                     if(originalAudio.thumbnail != unresolvedTrack.thumbnail) originalAudio.thumbnail = unresolvedTrack.thumbnail;
                     return originalAudio;
@@ -181,14 +181,14 @@ class TrackUtils {
                     (track.duration <= (unresolvedTrack.duration + 1500)));
                 if (sameDuration) {
                     sameDuration.uri = unresolvedTrack.uri;
-                    if(sameDuration.title == 'Unknown title' && sameDuration.title != unresolvedTrack.title) sameDuration.title = unresolvedTrack.title;
+                    if((sameDuration.title == 'Unknown title' || sameDuration.title == "Unspecified description") && sameDuration.title != unresolvedTrack.title) sameDuration.title = unresolvedTrack.title;
                     if(sameDuration.author != unresolvedTrack.author) sameDuration.author = unresolvedTrack.author;
                     if(sameDuration.thumbnail != unresolvedTrack.thumbnail) sameDuration.thumbnail = unresolvedTrack.thumbnail;
                     return sameDuration;
                 }
             }
             res.tracks[0].uri = unresolvedTrack.uri;
-            if(res.tracks[0].title == 'Unknown title' && unresolvedTrack.title != res.tracks[0].title) res.tracks[0].title = unresolvedTrack.title;
+            if((res.tracks[0].title == 'Unknown title' || res.tracks[0].title == "Unspecified description") && unresolvedTrack.title != res.tracks[0].title) res.tracks[0].title = unresolvedTrack.title;
             if(unresolvedTrack.author != res.tracks[0].author) res.tracks[0].author = unresolvedTrack.author;
             if(unresolvedTrack.thumbnail != res.tracks[0].thumbnail) res.tracks[0].thumbnail = unresolvedTrack.thumbnail;
             
