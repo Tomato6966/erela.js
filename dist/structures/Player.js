63091a2c140643435c9b3514a0811d8e0bb5f00c
Tomato6966 <chris.pre03@gmail.com>
2022-09-19 07:53:24 +0200

    added this.

--- dist/structures/Player.js
+++ dist/structures/Player.js
@@ -172,13 +172,11 @@ class Player {
     }
     deepFreeze(o) {
         Object.freeze(o);
-      
         Object.getOwnPropertyNames(o).forEach(function(prop) {
-             if (o.hasOwnProperty(prop) && o[prop] !== null && (typeof o[prop] === "object" || typeof o[prop] === "function") && !Object.isFrozen(o[prop])) {
-              deepFreeze(o[prop]);
+            if (o.hasOwnProperty(prop) && o[prop] !== null && (typeof o[prop] === "object" || typeof o[prop] === "function") && !Object.isFrozen(o[prop])) {
+              this.deepFreeze(o[prop]);
             }
         });
-      
         return o;
     }
     resetFilters() {
