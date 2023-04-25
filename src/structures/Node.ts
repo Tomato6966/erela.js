2b954495bb42712128f5f0a915981d9755f1e764
Tomato6966 <chris.pre03@gmail.com>
2023-04-25 15:23:09 +0200

    insta filter fix - fix (TS)

--- src/structures/Node.ts
+++ src/structures/Node.ts
@@ -550,8 +550,9 @@ export class Node {
                   player.filterUpdated++;
                   const maxMins = 8;
                   const currentDuration = player?.queue?.current?.duration || 0;
-                  if(currentDuration <= maxMins*60_000) {
-                    if(player.filterUpdated >= 3) {
+                  if(currentDuration <= maxMins*60_000 || isAbsolute(player?.queue?.current?.uri)) {
+                    let maxSize = interValSelfCounter > 400 ? 2 : 3;
+                    if(player.filterUpdated >= maxSize) {
                         player.filterUpdated = 0;
                         player.seek(player.position);
                     }
