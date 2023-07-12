7ee429dfffeeb45917fd45a0d4ee4eb367b3034d
CustomTag <65868765+CustomTag@users.noreply.github.com>
2023-07-12 22:57:09 +0200

    Update Utils.ts
    Making changes here also to the Thumbnail for Youtube

--- src/structures/Utils.ts
+++ src/structures/Utils.ts
@@ -112,7 +112,7 @@ export abstract class TrackUtils {
             typeof data.info.image === "string" ?
               data.info.image :
               ["youtube.", "youtu.be"].some(d => data.info.uri?.includes?.(d)) ?
-                `https://img.youtube.com/vi/${data.info.identifier}/mqdefault.jpg`
+                `https://img.youtube.com/vi/${data.info.identifier}/maxresdefault.jpg`
                 : (data.info?.md5_image && data.info?.uri?.includes?.("deezer"))
                   ? `https://cdns-images.dzcdn.net/images/cover/${data.info.md5_image}/500x500.jpg`
                   : null,
@@ -127,7 +127,7 @@ export abstract class TrackUtils {
             typeof data.info.image === "string" ? 
                 data.info.image :
                 ["youtube.", "youtu.be"].some(d => data.info.uri?.includes?.(d)) ?
-                    `https://img.youtube.com/vi/${data.info.identifier}/mqdefault.jpg`
+                    `https://img.youtube.com/vi/${data.info.identifier}/maxresdefault.jpg`
                     : (data.info?.md5_image && data.info?.uri?.includes?.("deezer"))
                         ? `https://cdns-images.dzcdn.net/images/cover/${data.info.md5_image}/500x500.jpg`
                         : null,
